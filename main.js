536a47d85cf44b03ffb1c5a1ecfef4563cb32d85
StanMilo <39526868+StanMilo@users.noreply.github.com>
2018-07-19 16:59:05 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded",function(){
 
+	window.PubSub = new SimplePubSub()
+
+	PubSub.on('user_selected', (user) => {
+        new TodosController(user)
+	})
+
 	new UsersController()
 
 // localStorage.setItem('usersList', JSON.stringify(USERS_LIST));
